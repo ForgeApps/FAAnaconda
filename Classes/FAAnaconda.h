3f25fd012ef8ff9b00b69e9d933934adc385b538
jeffmcfadden <jeff@forgeapps.com>
2014-05-22 14:11:10 -0700

    Initial.

---
+++ Classes/FAAnaconda.h
@@ -0,0 +1,17 @@
+//
+//  BFAPI.h
+//  Uncommen
+//
+//  Created by Jeff McFadden on 4/23/14.
+//  Copyright (c) 2014 Brushfire. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AFNetworking/AFNetworking.h>
+#import <AFNetworking/AFNetworkActivityIndicatorManager.h>
+
+@interface FAAnaconda : NSObject
+
+- (id)initWithAPIBaseURL:(NSURL *)apiBaseURL;
+
+@end
---
+++ Classes/FAAnaconda.m
@@ -0,0 +1,30 @@
+//
+//  BFAPI.m
+//  Uncommen
+//
+//  Created by Jeff McFadden on 4/23/14.
+//  Copyright (c) 2014 Brushfire. All rights reserved.
+//
+
+#import "FAAnaconda.h"
+
+@interface FAAnaconda()
+
+@property (strong) NSURL *apiBaseURL;
+
+@end
+
+@implementation FAAnaconda
+
+- (id)initWithAPIBaseURL:(NSURL *)apiBaseURL
+{
+    self = [super init];
+    if (self) {
+        _apiBaseURL = apiBaseURL;
+    }
+    return self;
+}
+
+
+
+@end
